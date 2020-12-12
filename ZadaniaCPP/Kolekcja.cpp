cb9fdc9ac2d12c5d5c746313264222d252fc6f8b
Andrzej Nowik <andrzej@mat.ug.edu.pl>
2020-12-12 09:52:15 +0100

    Rozne wersje wypisywania elementow wektora

--- ZadaniaCPP/Kolekcja.cpp
+++ ZadaniaCPP/Kolekcja.cpp
@@ -12,8 +12,22 @@ Kolekcja::Kolekcja()
 void Kolekcja::wypisanie()
 {
 	/*ró¿ne wersje pêtli po elementach kolekcji celem wypisania:*/
+
 	//wersja ,,archaiczna":
-	cout << "Elementy wektora vec: " << endl;
-	for (int i = 0; i < vec.size(); i++)
-		cout << vec[i] << endl;
+	//cout << "Elementy wektora vec (petla for po int): " << endl;
+	//for (int i = 0; i < vec.size(); i++)
+	//	cout << vec[i] << endl;
+
+	/*inna wersja, korzystaj¹ca z tzw. iteratorów (to takie a la wskaŸniki
+	ale dotycz¹ce tylko kolekcji), równie¿ doœæ archaiczna */
+	//cout << "Elementy wektora vec (petla po iteratorach): " << endl;
+	//for (/*vector<string>::iterator*/auto ite = vec.begin(); ite != vec.end(); ite++) {
+	//	cout << *ite << endl;
+	//}
+
+	/*wersja ,,wspó³czesna"*/
+	cout << "Elementy wektora vec (petla foreach): " << endl;
+	for (const auto &ele : vec) //tak ,,lepiej'', ale 
+		//mo¿na po prostu tak: for (auto ele : vec)
+		cout << ele << endl;
 }
