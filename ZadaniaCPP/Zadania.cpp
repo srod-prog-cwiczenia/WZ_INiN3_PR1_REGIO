972b1c7e00284306b050010fef28b47008edf602
Andrzej Nowik <andrzej@mat.ug.edu.pl>
2020-12-12 11:17:57 +0100

    3 sposob przekazywania

--- ZadaniaCPP/Zadania.cpp
+++ ZadaniaCPP/Zadania.cpp
@@ -13,7 +13,11 @@ void f1(int p) { //funkcja ,,wisz¹ca'' (nie przynale¿y do ¿adnej klasy o zgrozo 
 }
 void f2(int *p) { //przekazywanie przez wskaŸnik (konstrukcja czêsta w ANSI C):
 	cout << "Przekazywanie przez wskaŸnik: " << *p << endl;
-	*p += 3;
+	 *p += 3;
+}
+void f3(const int& p) { //prawdziwy tryb przekazywania parametru przez zmienn¹, czêsty w C++ (nie ma go w ANSI C+)
+	cout << "Przekazywanie przez wskaŸnik: " << p << endl;
+	p += 3;
 }
 void Zadania::zadanieNr2()
 {
@@ -25,5 +29,8 @@ void Zadania::zadanieNr2()
 	i = 10;
 	f2(&i);
 	cout << "Wartosc i po wyjsciu z funkcji f2: " << i << endl;
-
+	//prawdziwe przekazywanie przez zmienn¹
+	i = 10;
+	f3(i);
+	cout << "Wartosc i po wyjsciu z funkcji f3: " << i << endl;
 }
